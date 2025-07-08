e9ae8f5aae0fa7c624ca8a520179a62d8e791c19
roeeziv <roeeziv7@gmail.com>
2025-07-08 14:39:44 +0300

    changes for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/blabber.site/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
